dba7f24179842549f859fc942d3691656ba38728
Safi Anouar <anouar.safi@etu.univ-orleans.fr>
2024-03-05 15:42:13 +0100

    Modifier le fichier resume.pdf et les composants About et Home

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,5 +1,64 @@
 import React from "react";
 
+const AboutHeader = () => {
+  return (
+    <div className="pb-8">
+      <p className="text-4xl font-bold inline border-b-4 border-gray-500">
+        About
+      </p>
+    </div>
+  );
+};
+
+const PersonalInfo = () => {
+  return (
+    <div className="text-xl mt-6 md:mt-20">
+      <p>
+        Je suis Anouar Safi, un étudiant de 20 ans en troisième année de licence
+        informatique, spécialisé en ingénierie, à l'Université d'Orléans.
+        Actuellement en pleine réflexion sur ma spécialité future, oscillant
+        entre le développement web et le domaine de la data, je suis déterminé à
+        orienter ma carrière vers l'informatique.
+      </p>
+    </div>
+  );
+};
+
+const ProfessionalInfo = () => {
+  return (
+    <div className="text-xl">
+      <p>
+        À la recherche d'une opportunité de stage de trois mois dans n'importe
+        quel domaine informatique, je suis prêt à débuter dès avril 2024. La
+        possibilité de prolonger ce stage jusqu'au 31 juillet 2024 constitue une
+        chance d'approfondir mes connaissances et d'explorer davantage le monde
+        professionnel.
+      </p>
+      <p>
+        Parallèlement, mes projets incluent la recherche d'une alternance pour
+        mon master, prévu pour la rentrée de septembre 2025. Je suis convaincu
+        que cette expérience contribuera grandement à l'enrichissement de mes
+        compétences et à ma croissance professionnelle.
+      </p>
+    </div>
+  );
+};
+
+const HobbiesInfo = () => {
+  return (
+    <div className="text-xl">
+      <p>
+        En dehors de ma vie académique, je suis un passionné de jeux vidéo,
+        ardent gamer et compétiteur, particulièrement doué à FIFA et Call of
+        Duty. Mon amour pour le football s'exprime également sur le terrain en
+        tant que joueur de l'équipe universitaire de futsal de l'Université
+        d'Orléans. Ces passions nourrissent mon esprit compétitif et ma
+        détermination à exceller dans tous les aspects de ma vie.
+      </p>
+    </div>
+  );
+};
+
 const About = () => {
   return (
     <div
@@ -7,37 +66,11 @@ const About = () => {
       className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
     >
       <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
-        <div className="pb-8">
-          <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-            About
-          </p>
-        </div>
-
-        <div className="text-xl mt-6 md:mt-20">
-          <p>
-            Je m'appelle Anouar SAFI et je suis actuellement étudiant en 3ème
-            année de Licence Informatique à l'université d'Orléans. Je vous
-            adresse la présente pour soumettre ma candidature en vue d'un
-            stage de 3 mois au sein de votre entreprise, à partir d'avril 2024.
-            Passionné par le développement web, le traitement de données,
-            l'intelligence artificielle et la cybersécurité, je suis
-            particulièrement enthousiaste à l'idée de rejoindre votre équipe
-            et de contribuer à vos projets innovants.
-          </p>
-        </div>
-
+        <AboutHeader />
+        <PersonalInfo />
         <br />
-
-        <div className="text-xl">
-          <p>
-            Mes compétences techniques en Java, Python, HTML, CSS, JavaScript,
-            SQL, PHP, C/C++, et OCAML, acquises au cours de ma formation et de
-            mes projets antérieurs, me permettent d'apporter une contribution
-            significative à vos activités. Ma capacité à travailler aussi bien en
-            autonomie qu'en équipe, ainsi que mon aptitude à m'adapter à divers
-            environnements, font de moi un candidat polyvalent et motivé.
-          </p>
-        </div>
+        <ProfessionalInfo />
+        <HobbiesInfo />
       </div>
     </div>
   );
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,19 +5,14 @@ import { Link } from "react-scroll";
 
 const Home = () => {
   return (
-    <div
-      name="home"
-      className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800"
-    >
-      <div className="max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row">
+    <div name="home" className="h-screen w-full bg-gradient-to-b from-black via-black to-gray-800">
+      <div className="max-w-screen-lg mx-auto flex flex-col md:flex-row items-center justify-center h-full px-4">
         <div className="flex flex-col justify-center h-full mb-8 md:mb-0 md:mr-4 md:w-1/2">
           <h2 className="text-4xl sm:text-7xl font-bold text-white">
             Étudiant en Informatique
           </h2>
           <p className="text-gray-500 py-4 max-w-md">
-            Je suis passionné par le monde de l'informatique et spécialisé dans
-            le développement logiciel et web. Explorez mon portfolio pour
-            découvrir mes compétences, projets et aspirations professionnelles.
+            Passionné par l'univers de l'informatique, je me consacre au développement logiciel et explore divers domaines technologiques. Parcourez mon portfolio pour découvrir mes compétences, projets et aspirations professionnelles.
           </p>
 
           <div>
@@ -48,4 +43,3 @@ const Home = () => {
 };
 
 export default Home;
-
