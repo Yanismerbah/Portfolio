61a71a8f8fb59f861651f03569c8584d7f4a9714
Safi Anouar <anouar.safi@etu.univ-orleans.fr>
2024-03-05 16:46:59 +0100

    Message de commit des modifications apportées à About.jsx et index.css

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -63,12 +63,11 @@ const About = () => {
   return (
     <div
       name="about"
-      className="w-full h-screen bg-gradient-to-b from-gray-800 to-black text-white"
+      className="w-full bg-gradient-to-b from-gray-800 to-black text-white p-4"
     >
-      <div className="max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full">
+      <div className="max-w-screen-md mx-auto">
         <AboutHeader />
         <PersonalInfo />
-        <br />
         <ProfessionalInfo />
         <HobbiesInfo />
       </div>
@@ -77,3 +76,4 @@ const About = () => {
 };
 
 export default About;
+
