55c9987b9731a980369102354efcaa280935555a
Yanis MERBAH <yanis.merbah2021@gmail.com>
2025-01-30 00:36:46 +0100

    changement de text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ const AboutHeader = () => {
   return (
     <div className="pb-8">
       <p className="text-4xl font-bold inline border-b-4 border-gray-500">
-        About
+        About me
       </p>
     </div>
   );
