3b9feba9d08d975189df8d7c72464abef7857d7b
Yanis MERBAH <yanis.merbah2021@gmail.com>
2025-01-30 00:32:01 +0100

    changement de text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const AboutHeader = () => {
   return (
     <div className="pb-8">
-      <p className="text-4xl font-bold inline  border-gray-500">
+      <p className="text-4xl font-bold inline border-b-4 border-gray-500">
         About
       </p>
     </div>
